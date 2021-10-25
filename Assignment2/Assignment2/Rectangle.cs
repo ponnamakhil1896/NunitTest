1909d89d9cb660a54f1070cdec37b8d59dd856e7
Akhil <ponnamakhil1896@gmail.com>
2021-10-25 15:37:42 -0400

    added rectangle class

---
+++ Assignment2/Assignment2/Rectangle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Rectangle
+    {
+        private int len;
+        private int wid;
+        private int hgt;
+
+        public Rectangle()
+        {
+            this.len = 1;
+            this.wid = 1;
+            this.hgt = 1;
+        }
+        public Rectangle(int value)
+        {
+            this.len = value;
+            this.wid = value;
+            this.hgt = value;
+        }
+
+        public int GetLength()
+        {
+
+            return this.len;
+        }
+
+        public void SetLength(int length)
+        {
+            this.len = length;
+            Console.WriteLine("length set to " + length);
+        }
+
+        public int GetWidth()
+        {
+
+            return this.wid;
+        }
+
+        public void SetWidth(int width)
+        {
+            this.wid = width;
+            Console.WriteLine("width set to " + width);
+        }
+        public int GetHeight()
+        {
+
+            return this.hgt;
+        }
+
+        public void SetHeight(int height)
+        {
+            this.hgt = height;
+            Console.WriteLine("height set to " + height);
+        }
+
+        public double GetVolume()
+        {
+            double volume = (this.len * this.wid * this.hgt);
+            Console.WriteLine("Volume is ");
+            return volume;
+        }
+}
+}
